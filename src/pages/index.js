7976fb8bfa1f4b8932d6887498b978f35ac2e10e
Guibson Moura <mouraguibson12@gmail.com>
2022-06-01 11:25:42 -0300

    first commit

---
+++ src/pages/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import {Button} from "@mui/material"
+
+export default function Home() {
+  return (
+    <div>
+      jasdfç
+      <Button variant='outlined'>alguma coisa </Button>
+      </div>
+  )
+}
